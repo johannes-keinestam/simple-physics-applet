97bdad59cd3fe19e7d16941bf2f8866d54a0b797
johannes.keinestam@gmail.com <johannes.keinestam@gmail.com@70a12ff8-017a-2723-47e7-cb3d141b3f65>
2012-04-14 19:42:22 +0000

    Implemented Gravity Model (except the actual gravity part) and KeyListener. Should now work.

--- trunk/src/Ball.java
+++ trunk/src/Ball.java
@@ -1,23 +1,21 @@
 import java.awt.geom.Ellipse2D;
 
 public class Ball {
-    private final Ellipse2D ellipse;
-    // x, y: positions
-    // vx, vy: speeds/directions
-    // r: ball radius
-    private final double radius, x, y, vx, vy;
+	// x, y: positions
+	// vx, vy: speeds/directions
+	// r: ball radius
+	public double radius, x, y, vx, vy;
 
-    public Ball(double radius, double x, double y) {
-        this.radius = radius;
-        this.x = x;
-        this.y = y;
-        this.vx = 2.3;
+	public Ball(double radius, double x, double y) {
+		this.radius = radius;
+		this.x = x;
+		this.y = y;
+		this.vx = 2.3;
 		this.vy = 1;
-        
-        ellipse = new Ellipse2D.Double(x-radius, y-radius, 2*radius, 2*radius);
-    }
-    
-    public Ellipse2D getEllipse() {
-        return ellipse;
-    }
+	}
+
+	public Ellipse2D getEllipse() {
+		return new Ellipse2D.Double(x - radius, y - radius, 2 * radius,
+				2 * radius);
+	}
 }
